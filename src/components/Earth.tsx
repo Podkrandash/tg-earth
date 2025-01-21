ddacb38cb3b6cb990b9dc97b72a364a6fe6d165a
Podkrandash <annenskov235@gmail.com>
2025-01-22 01:02:47 +0400

    Fix TypeScript configuration

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,7 +1,8 @@
 import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { Sphere } from '@react-three/drei';
+import { Sphere, OrbitControls } from '@react-three/drei';
 import * as THREE from 'three';
+import Atmosphere from './Atmosphere';
 
 const Earth: React.FC = () => {
   const earthRef = useRef<THREE.Mesh>(null);
@@ -19,16 +20,32 @@ const Earth: React.FC = () => {
 
   return (
     <>
-      <Sphere ref={earthRef} args={[1, 64, 64]}>
-        <meshPhysicalMaterial
-          map={colorMap}
-          normalMap={normalMap}
-          roughnessMap={roughnessMap}
-          normalScale={new THREE.Vector2(0.2, 0.2)}
-          roughness={0.7}
-          metalness={0.1}
-        />
-      </Sphere>
+      <OrbitControls
+        enableZoom={true}
+        enablePan={false}
+        minDistance={2}
+        maxDistance={7}
+        rotateSpeed={0.5}
+        onStart={() => {
+          if (earthRef.current) earthRef.current.userData.isDragging = true;
+        }}
+        onEnd={() => {
+          if (earthRef.current) earthRef.current.userData.isDragging = false;
+        }}
+      />
+      <group>
+        <Atmosphere />
+        <Sphere ref={earthRef} args={[1, 64, 64]}>
+          <meshPhysicalMaterial
+            map={colorMap}
+            normalMap={normalMap}
+            roughnessMap={roughnessMap}
+            normalScale={new THREE.Vector2(0.2, 0.2)}
+            roughness={0.7}
+            metalness={0.1}
+          />
+        </Sphere>
+      </group>
     </>
   );
 };
